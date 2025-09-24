273a65b9aa8a16c6e92804eb19c3ad7c78b39baf
Rivo Rahajason <gasy.starttask@gmail.com>
2025-09-24 19:38:36 +0200

    fix(app): ⚠ Warning: Next.js inferred your workspace root, but it may not be correct.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: join(__dirname, "../blog-cms"),
+  }
 };
 
 export default nextConfig;
